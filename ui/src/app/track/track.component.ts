e9929473e3847f5e0d6aa9339f0b28b1c8e06fd0
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2017-02-24 16:38:31 +0200

    Map routes

--- ui/src/app/track/track.component.ts
+++ ui/src/app/track/track.component.ts
@@ -1,18 +1,42 @@
 import {Component, OnInit} from '@angular/core';
 
+import {Http, Headers} from '@angular/http';
+
 @Component({
     selector: 'app-track',
     templateUrl: './track.component.html',
     styleUrls: ['./track.component.css']
 })
 export class TrackComponent implements OnInit {
+    public lat;
+    public lng;
+    public polyLines = [];
 
-    constructor() {
+    constructor(private http: Http,) {
+        this.http = http;
     }
 
     ngOnInit() {
-    }
+        this.lat = -33.924158;
+        this.lng = 18.454311;
 
-    lat: number = -33.924158;
-    lng: number = 18.454311;
+        this.http.get("assets/journey.json")
+            .map(response => response.json())
+            .subscribe((response) => {
+                for (let itinerary of response.itineraries) {
+                    let coords = [];
+                    for (let leg of itinerary.legs) {
+                        if (leg.type == "Walking") {
+                            coords.concat(leg.geometry.coordinates)
+                        } else if (leg.type == "Transit") {
+                            for (let waypoint of leg.waypoints) {
+                                coords.push(
+                                    waypoint.stop.geometry.coordinates)
+                            }
+                        }
+                    }
+                    this.polyLines.push(coords);
+                }
+            })
+    }
 }
